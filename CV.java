1058f7c5ab39dcdcb3bc6fc24b79fdeea62215f6
ChaseYUL99 <chase.yul99@gmail.com>
2018-02-05 10:06:23 -0500

    Création de la structure et des champs d'instance

---
+++ CV.java
@@ -0,0 +1,30 @@
+// Auteurs : Julien Cardinal, Paul Sasu
+
+public class CV {
+	
+	private String nom;
+	private String prenom;
+	private String formation;
+	private int expTrav;
+	private String[] comps;
+	private String attentes;
+	
+	public CV( String nom, String prenom, String formation, int expTrav, String[] comps, String attentes ) {		
+		
+		
+		
+	}
+
+	public static void main( String[] args ) {
+		
+		
+		
+	}
+	
+	public void affiche() {
+		
+		
+		
+	}
+
+}
